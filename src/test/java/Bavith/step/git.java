4ced0778cdc702144f00ee6662684193cf49b95f
bavith-vemana <74925266+bavith-vemana@users.noreply.github.com>
2024-02-12 16:16:45 +0530

    Update git.java

--- src/test/java/Bavith/step/git.java
+++ src/test/java/Bavith/step/git.java
@@ -1,12 +1,1 @@
-package Bavith.step;
 
-public class git {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("jhdsvkjh");
-		System.out.println("aaaaaaaaa");
-		System.out.println("bbbbbbbbbb");
-	}
-
-}
